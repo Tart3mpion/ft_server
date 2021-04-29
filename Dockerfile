2d7bb6925b8fe6d4b0c291a10c4d988ed4b38764
Lucrèce des Courtis <ldes-cou@student.42.fr>
2021-04-29 11:02:32 +0200

    yo

--- Dockerfile
+++ Dockerfile
@@ -2,18 +2,23 @@ FROM debian:buster
 
 LABEL ldes-cou (ldes-cou@student.42.fr)
 
-RUN apt-get update
-RUN apt-get install -y nginx
-RUN apt-get install openssl
+#WORKDIR /ft_server
+
+RUN apt-get update && apt-get install -y nginx 
+RUN apt-get install -y openssl
 RUN apt-get install -y mariadb-server
 RUN apt-get install -y wget
+RUN apt-get -y install php7.3-fpm php7.3-common php7.3-mysql php7.3-gmp php7.3-curl php7.3-intl php7.3-mbstring php7.3-xmlrpc php7.3-gd php7.3-xml php7.3-cli php7.3-zip php7.3-soap php7.3-imap
+RUN apt-get -y install wget
 RUN apt-get install -y vim
 
 COPY srcs/nginx.conf ./
 COPY srcs/infos.php ./
 COPY srcs/config.sh ./
+COPY srcs/config.php ./
+COPY srcs/wp-config.php ./
 
 EXPOSE 80
 EXPOSE 443
-CMD bash /config.sh
+CMD bash ./config.sh
 
--- srcs/config.sh
+++ srcs/config.sh
@@ -1,34 +1,48 @@
+
 service mysql start
 
 #Rights
+
 chown -R www-data /var/www/*
 chmod -R 755 /var/www/*
 
 #website folder
-mkdir -p var/www/localhost
-cp ./infos.php var/www/localhost/
+mkdir -p var/www/localhost/
+cp ./infos.php var/www/localhost/index.php
 
 #Nginx
 mv ./nginx.conf /etc/nginx/sites-available/
 ln -s /etc/nginx/sites-available/nginx.conf /etc/nginx/sites-enabled/
+rm -rf /etc/nginx/sites-enabled/default
 
 #openSSL
-mkdir /etc/nginx/ssl
-openssl req -newkey rsa:4096 -x509 -sha256 -days 3650 -nodes -out etc/nginx/ssl/localhost.pem -keyout etc/nginx/ssl/localhost.key -subj "/C=FR/ST=IledeFrance/L=LParis/O=Ecole/OU=42 Department/CN=www.localhost.com"
+mkdir /etc/nginx/ssl/
+openssl req -newkey rsa:4096 -x509 -sha256 -days 3650 -nodes -out /etc/nginx/ssl/localhost.pem -keyout /etc/nginx/ssl/localhost.key -subj "/C=FR/ST=IledeFrance/L=LParis/O=Ecole/OU=42 Department/CN=www.localhost.com"
+ 
+
 
 #phpmyadmin
-wget https://files.phpmyadmin.net/phpMyAdmin/4.9.7/phpMyAdmin-4.9.7-all-languages.tar.gz 
-tar xvf phpMyAdmin-4.9.7-all-languages.tar.gz 
-mv phpMyAdmin-4.9.7-all-languages/ /usr/share/phpmyadmin
-cp /usr/share/phpmyadmin/config.sample.inc.php /usr/share/phpmyadmin/config.inc.php
+#mkdir /var/www/localhost/phpmyadmin
+#wget https://files.phpmyadmin.net/phpMyAdmin/4.9.7/phpMyAdmin-4.9.7-all-languages.tar.gz
+#tar xvf phpMyAdmin-4.9.7-all-languages.tar.gz
+#mv phpMyAdmin-4.9.7-all-languages/ /usr/share/phpmyadmin
+#cp ./config.php /var/www/localhost/phpmyadmin/
+
+
 
 
 #mysql
 #mysql_secure_installation
-mariadb < /usr/share/phpmyadmin/sql/create_tables.sql
-#echo "GRANT SELECT, INSERT, UPDATE, DELETE ON phpmyadmin.* TO 'pma'@'localhost' IDENTIFIED BY 'password';"
-#echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'password' WITH GRANT OPTION;"
-#echo "exit"
+#echo "CREATE DATABASE wordpress;" | mysql -u root --skip-password
+#echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'%' WITH GRANT OPTION;" | mysql -u root --skip-password
+#echo "update mysql.user set plugin='mysql_native_password' where user='root';" | mysql -u root --skip-password
+#echo "FLUSH PRIVILEGES;" | mysql -u root --skip-password
+
+# wordpress
+#wget -c https://wordpress.org/latest.tar.gz
+#tar -xvzf latest.tar.gz
+#mv ./wordpress/ /var/www/localhost/
+#cp ./wp-config.php /var/www/localhost/wordpress/
 
 service php7.3-fpm start
 service nginx start
